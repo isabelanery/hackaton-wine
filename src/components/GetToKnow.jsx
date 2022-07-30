b904d1b9f52bc8091c0071c335a0e1c87bfd804f
cathe <cacateles12@gmail.com>
2022-07-30 20:20:32 -0300

    Update: Adicionada as figuras do getToKnow

--- src/components/GetToKnow.jsx
+++ src/components/GetToKnow.jsx
@@ -3,11 +3,20 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
 
+import fig1 from '../images/carousel1.png';
+import fig2 from '../images/carousel2.png';
+import fig3 from '../images/carousel3.png';
+import fig4 from '../images/carousel4.png';
+import fig5 from '../images/carousel5.png';
+
 function GetToKnow() {
   return (
     <Slider dots infinite speed={500} slidesToShow={1} slidesToScroll={1}>
-      <div>1</div>
-      <div>2</div>
+      <div><img src={fig1} alt="" /></div>
+      <div><img src={fig2} alt="" /></div>
+      <div><img src={fig3} alt="" /></div>
+      <div><img src={fig4} alt="" /></div>
+      <div><img src={fig5} alt="" /></div>
     </Slider>
   );
 }
